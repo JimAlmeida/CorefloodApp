fd88a97782f03fe5c6cebe048d3d3a345d5860e1
Thiago <thiagocdac@gmail.com>
2021-06-22 14:54:29 -0300

    Initial Release

---
+++ CorefloodApp/Components/ProgressBarElements.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorefloodApp
+{
+    public static class ProgressBarElements
+    {
+        public static readonly MarkupString AddedRockInput = new("<div class=\"progress-bar\" role=\"progressbar\" style=\"width: 25%\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\">Added Rock Arguments!</div>");
+        public static readonly MarkupString AddedSimInput = new("<div class=\"progress-bar\" role=\"progressbar\" style=\"width: 25%\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\">Added Simulation Arguments!</div>");
+        public static readonly MarkupString StartedSimulation = new("<div class=\"progress-bar bg-warning\" role=\"progressbar\" style=\"width: 25%\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\">Started Simulation!</div>");
+        public static readonly MarkupString FinishedSimulation = new("<div class=\"progress-bar bg-success\" role=\"progressbar\" style=\"width: 25%\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\">Finished Simulation!</div>");
+    }
+}
--- CorefloodApp/Form1.cs
+++ CorefloodApp/Form1.cs
@@ -8,11 +8,12 @@ using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
 
+using MatBlazor;
+
 namespace CorefloodApp
 {
     public partial class Form1 : System.Windows.Forms.Form
     {
-    
         public Form1()
         {
             InitializeComponent();
@@ -27,8 +28,11 @@ namespace CorefloodApp
             .AddBootstrapProviders()
             .AddFontAwesomeIcons();
 
+            serviceCollection.AddMatBlazor();
+
             serviceCollection.AddBlazorWebView();
             
+
             var blazor = new BlazorWebView()
             {
                 Dock = DockStyle.Fill,
@@ -36,8 +40,7 @@ namespace CorefloodApp
                 Services = serviceCollection.BuildServiceProvider(),
             };
 
-            blazor.RootComponents.Add<SimForm>("#form");
-            blazor.RootComponents.Add<ChartScreen>("#chartscreen");
+            blazor.RootComponents.Add<Index>("#app");
           
             Controls.Add(blazor);
         }
